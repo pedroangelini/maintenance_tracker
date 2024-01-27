875cdf3db60348dfeb2e99b971e117071a1939ed
pedroangelini <pedro.angelini@gmail.com>
2024-01-27 11:52:00 +0100

    WIP MaintenanceTracker and associated tests

---
+++ maintenance_tracker.py
@@ -0,0 +1,151 @@
+import logging
+from core import (
+    Task,
+    TaskLister,
+    TaskListPersister,
+    Action,
+    ActionLister,
+    ActionListPersister,
+    Ordering,
+)
+from enum import Enum
+from datetime import datetime, UTC
+
+
+class ActionRecordResults(Enum):
+    SUCCESS = 1  # all good, and task of this action matches the task already registered
+    TASK_MISMATCH = 2  # action was recorded, but the task referenced in the action is not equals to the one already registered
+    FAILURE = (
+        0  # for some reason we couldn't add the task, but did not raise an exception
+    )
+
+
+class MaintenanceTracker:
+    """maintenance_tracker sets up lists of tasks and actions related to those tasks."""
+
+    def __init__(
+        self, load=False, save_dir=None, save_actions_file=None, save_task_file=None
+    ):
+        self.task_list = TaskLister([])
+        self.action_list = ActionLister([])
+
+        self.task_list_saver = TaskListPersister(
+            self.task_list, save_dir, save_task_file
+        )
+        self.action_list_saver = ActionListPersister(
+            self.action_list, save_dir, save_actions_file
+        )
+
+        if load:
+            self.task_list_saver.load()
+            self.action_list_saver.load()
+
+    def register_task(self, new_task: Task):
+        self.task_list.append(new_task)
+
+    def record_run(self, new_action: Action) -> ActionRecordResults:
+        """Records that an action was performed,
+        Ensures the referenced task is in the task_list, and checks if the task in the
+        task_list matches the one passed in the action
+
+        Args:
+            new_action (action): new action to be recorded
+
+        Returns:
+            ActionRecordResults: code with information about the registration result
+              - FAILURE if could not add the action
+              - TASK_MISMATCH if the action was added, but the task referenced in the
+              action is not equals to the one in the task_list
+              - SUCCESS if the action was added successfully with a matching task
+
+        """
+        ret_code = ActionRecordResults.FAILURE
+
+        # check if we've seen this task before, if not, register it
+        if self.task_list._check_task_name_available(new_action.ref_task.name):
+            logging.info(
+                f"adding an action to a task that did not exist before - {new_action.ref_task.name}"
+            )
+            self.register_task(new_action.ref_task)
+        else:
+            self.action_list.append(new_action)
+
+            # warn the user if ref_task is different from the one in the list
+            registered_task = self.task_list.get_task_by_name(new_action.ref_task.name)
+            if registered_task == new_action.ref_task:
+                ret_code = ActionRecordResults.SUCCESS
+            else:
+                ret_code = ActionRecordResults.TASK_MISMATCH
+
+        return ret_code
+
+    def get_actions_for_task(
+        self, target_task: Task, ordered: Ordering | bool = False
+    ) -> ActionLister:
+        """gets a list of actions for a given task (based on the task name)
+
+        Args:
+            target_task (task): the task you want to list actions for
+            ordered (Ordering | bool, optional): should the returned action list be ordered based on the action date time? values: Ordering.ASC, Ordering.DESC or bool True is the same as Ordering.ASC. Defaults to False.
+
+        Returns:
+            action_lister: a list of actions for the desired task
+        """
+        result_list = [
+            action
+            for action in self.action_list
+            if action.ref_task.name == target_task.name
+        ]
+        if ordered:
+            result_list = sorted(
+                result_list,
+                key=lambda a: a.timestamp,
+                reverse=(ordered == Ordering.DESC),
+            )
+
+        return ActionLister(result_list)
+
+    def get_latest_task_run(self, tgt_task: Task) -> Action:
+        """Returns the most recent run of a task
+
+        Args:
+            tgt_task (task): the task we are looking for
+
+        Returns:
+            action: the latest action of the the reference task
+        """
+        actions = self.get_actions_for_task(tgt_task, ordered=Ordering.DESC)
+
+        return actions[0]
+
+    def check_overdue(self, task: Task) -> bool:
+        """Checks if a task is overdue.
+        A task is overdue if we have not had an action run between the lastest time the
+        task was programmed before today
+        Args:
+            task (Task): the task to check for
+
+        Returns:
+            bool: whether the task is overdue
+        """
+        last_programmed_time = task.get_programmed_time(-1)
+
+        if last_programmed_time is None:
+            # tasks without programmed time are never overdue
+            return False
+
+        last_run_time = self.get_latest_task_run(task).timestamp
+
+        return (
+            last_programmed_time < datetime.now(UTC)
+            and last_run_time < last_programmed_time
+        )
+
+    def get_next_due_time(self, task):
+        pass
+
+    def time_since_last_exec(self, task):
+        pass
+
+    def list_tasks(self, criteria, **kwargs):
+        pass
---
+++ test_maintenance_tracker.py
@@ -0,0 +1,84 @@
+import pytest
+from core import *
+from maintenance_tracker import *
+from datetime import datetime
+from test_core import task1, task2, task3  # fixtures
+
+
+def test_listing_actions_for_task(task1, task2):
+    action1_t1 = Action(
+        datetime(
+            2024,
+            1,
+            1,
+        ),
+        task1,
+        "ran task1 on new year day",
+        "me",
+    )
+    action2_t1 = Action(
+        datetime(
+            2024,
+            1,
+            2,
+        ),
+        task1,
+        "ran task1 on the second of the year",
+        "me",
+    )
+    action1_t2 = Action(
+        datetime(
+            2024,
+            1,
+            1,
+        ),
+        task2,
+        "ran task2 on new year day",
+        "me",
+    )
+    action2_t2 = Action(
+        datetime(
+            2024,
+            1,
+            2,
+        ),
+        task2,
+        "ran task2 on the second of the year",
+        "me",
+    )
+
+    action_lst = ActionLister([action1_t1, action1_t2, action2_t1, action2_t2])
+
+    mtnt = MaintenanceTracker()
+    mtnt.register_task(task1)
+    mtnt.register_task(task2)
+    for a in action_lst:
+        mtnt.record_run(a)
+
+    assert ActionLister([action1_t2, action2_t2]) == mtnt.get_actions_for_task(task2)
+
+
+def test_record_run(task1):
+    action1 = Action(
+        datetime(2024, 1, 7, 10, 15),
+        task1,
+        "running the first task",
+        "a description for the first run of task1",
+        "Pedro",
+    )
+    action2 = Action(
+        datetime(2024, 1, 7, 11, 15),
+        task1,
+        "running the first task again",
+        "a description for the Second of task 1",
+        "Pedro",
+    )
+
+    mtnt = MaintenanceTracker()
+    mtnt.record_run(action1)
+
+    assert mtnt.task_list[0].name == "my first task"
+
+
+if __name__ == "__main__":
+    pass
