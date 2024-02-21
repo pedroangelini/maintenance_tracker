f75af31bd32e461d1296eabfaa609b3843f0b392
pedroangelini <72536471+pedroangelini@users.noreply.github.com>
2024-02-21 23:50:28 +0100

    tests for MaintenanceTracker.get_latest_task_run and MaintenanceTracker.check_overdue

--- test_maintenance_tracker.py
+++ test_maintenance_tracker.py
@@ -3,6 +3,7 @@ from core import *
 from maintenance_tracker import *
 from datetime import datetime
 from test_core import task1, task2, task3  # fixtures
+from test_core import action1_t1, action2_t1  # fixtures
 
 
 def test_listing_actions_for_task(task1, task2):
@@ -71,6 +72,92 @@ def test_record_run(task1):
     mtnt.record_run(action1)
 
     assert mtnt.task_list[0].name == "my first task"
+
+
+def test_get_latest_task_run_success(task1, action1_t1, action2_t1):
+    mtnt = MaintenanceTracker()
+    mtnt.register_task(task1)
+    mtnt.record_run(action1_t1)
+    mtnt.record_run(action2_t1)
+
+    assert mtnt.get_latest_task_run(task1) == action2_t1
+
+
+def test_get_latest_task_run_no_action(task2):
+    mtnt = MaintenanceTracker()
+    mtnt.register_task(task2)
+
+    assert mtnt.get_latest_task_run(task2) is None
+
+
+def test_get_latest_task_run_empty(task2):
+    mtnt = MaintenanceTracker()
+
+    assert mtnt.get_latest_task_run(task2) is None
+
+
+def test_check_overdue_has_action_is_overdue(task1, action1_t1, action2_t1):
+    mtnt = MaintenanceTracker()
+    mtnt.register_task(task1)  # every hour at 32 min, start 2023-12-24
+    mtnt.record_run(action1_t1)  # new year 2024 (0h)
+    mtnt.record_run(action2_t1)  # 2024-01-02, 6 AM
+
+    when = datetime(2024, 1, 2, 6, 33, tzinfo=UTC)
+
+    assert mtnt.check_overdue(task1, when=when) == True
+
+
+def test_check_overdue_has_action_is_not_overdue(task1, action1_t1, action2_t1):
+    mtnt = MaintenanceTracker()
+    mtnt.register_task(task1)  # every hour at 32 min, start 2023-12-24
+    mtnt.record_run(action1_t1)  # new year 2024 (0h)
+    mtnt.record_run(action2_t1)  # 2024-01-02, 6 AM
+
+    when = datetime(2024, 1, 2, 6, 31, tzinfo=UTC)
+
+    assert mtnt.check_overdue(task1, when=when) == False
+
+
+def test_check_overdue_has_no_action_is_overdue(task1):
+    mtnt = MaintenanceTracker()
+    mtnt.register_task(task1)  # every hour at 32 min, start 2023-12-24
+
+    when = datetime(2024, 1, 2, 6, 31, tzinfo=UTC)
+
+    assert mtnt.check_overdue(task1, when=when) == True
+
+
+def test_check_overdue_has_no_action_is_not_overdue():
+    mtnt = MaintenanceTracker()
+    when = datetime(2024, 2, 21, 23, 24, tzinfo=UTC)
+    future_task = Task(
+        "future task",
+        "starts in the future",
+        datetime(2024, 2, 22, 23, 24, tzinfo=UTC),
+        timedelta(days=1),
+    )
+
+    mtnt.register_task(future_task)
+
+    assert mtnt.check_overdue(future_task, when) == False
+
+
+def test_check_overdue_only_as_action_after_when(task1):
+    mtnt = MaintenanceTracker()
+    mtnt.register_task(task1)  # every hour at 32 min, start 2023-12-24
+
+    when = datetime(2024, 1, 2, 6, 30, tzinfo=UTC)
+    future_action = Action(
+        datetime(2024, 1, 2, 6, 31, tzinfo=UTC),
+        task1,
+        "an action in the future",
+        "...in this context",
+        "test me",
+    )
+
+    mtnt.record_run(future_action)
+
+    assert mtnt.check_overdue(task1, when=when) == True
 
 
 if __name__ == "__main__":
